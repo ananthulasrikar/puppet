694a6fd4947c9244cbe1b2e44bd0dcc4884ae6c8
SrikarAnanthula <ananthulasrikar@gmail.com>
2016-12-24 19:31:34 +0530

    added user michell

--- modules/cron-puppet/manifests/init.pp
+++ modules/cron-puppet/manifests/init.pp
@@ -14,4 +14,11 @@ class cron-puppet {
         minute  => '*/30',
         require => File['post-hook'],
     }
+    user { 'mitchell':
+        ensure     => present,
+        uid        => '1000',
+        gid        => '1000',
+        shell      => '/bin/bash',
+        home       => '/home/mitchell'
+}
 }
