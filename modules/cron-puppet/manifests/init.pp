2f0676657a8e24e280a9186faa4c3e627c20265c
SrikarAnanthula <ananthulasrikar@gmail.com>
2016-12-24 19:39:30 +0530

    Update init.pp

--- modules/cron-puppet/manifests/init.pp
+++ modules/cron-puppet/manifests/init.pp
@@ -17,7 +17,6 @@ class cron-puppet {
     user { 'mitchell':
         ensure     => present,
         uid        => '1000',
-        gid        => '1000',
         shell      => '/bin/bash',
         home       => '/home/mitchell'
     }
