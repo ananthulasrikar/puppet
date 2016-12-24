aba93cea606ac14db66f9491e2eb2d8f2fe8d865
SrikarAnanthula <ananthulasrikar@gmail.com>
2016-12-24 19:33:44 +0530

    Update init.pp

--- modules/cron-puppet/manifests/init.pp
+++ modules/cron-puppet/manifests/init.pp
@@ -14,11 +14,4 @@ class cron-puppet {
         minute  => '*/30',
         require => File['post-hook'],
     }
-    user { 'mitchell':
-        ensure     => present,
-        uid        => '1000',
-        gid        => '1000',
-        shell      => '/bin/bash',
-        home       => '/home/mitchell'
-    }
 }
