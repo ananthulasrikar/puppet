a4666d2054c51a37b98973a9ac13ba286e552406
SrikarAnanthula <ananthulasrikar@gmail.com>
2016-12-24 20:09:28 +0530

    Delete user.pp

--- modules/cron-puppet/manifests/user.pp
+++
@@ -1,9 +0,0 @@
-class user-puppet {
-    user { 'mitchell':
-        ensure     => present,
-        uid        => '1000',
-        gid        => '1000',
-        shell      => '/bin/bash',
-        home       => '/home/mitchell'
-    }
-}
