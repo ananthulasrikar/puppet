a4bcac8f0849094d4439cee1b8f7284ff90b280e
SrikarAnanthula <ananthulasrikar@gmail.com>
2016-12-24 19:35:08 +0530

    Create user.pp

---
+++ modules/cron-puppet/manifests/user.pp
@@ -0,0 +1,9 @@
+class user-puppet {
+    user { 'mitchell':
+        ensure     => present,
+        uid        => '1000',
+        gid        => '1000',
+        shell      => '/bin/bash',
+        home       => '/home/mitchell'
+    }
+}
