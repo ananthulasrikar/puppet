b795c4d5a7d5ad0c1cff5d38269a6e1970ac0121
Srikar Ananthula <ananthulasrikar@gmail.com>
2018-06-11 13:19:41 +0530

    Fix java

--- modules/java-module/manifests/java.pp
+++ modules/java-module/manifests/java.pp
@@ -1,6 +1,6 @@
 class java-module::java {
-  
-class { '::java':
+
+class { 'java':
     distribution          => 'jdk',
     package               => 'java-1.8.0-openjdk-devel',
     java_alternative      => 'jre-1.8.0-openjdk.x86_64',
