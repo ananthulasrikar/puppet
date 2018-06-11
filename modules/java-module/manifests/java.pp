bd4524b51ceb122be3bfe04d74708df05525c33e
Srikar Ananthula <ananthulasrikar@gmail.com>
2018-06-11 13:20:45 +0530

    fix

--- modules/java-module/manifests/java.pp
+++ modules/java-module/manifests/java.pp
@@ -1,9 +1,6 @@
-class java-module::java {
-
 class { 'java':
     distribution          => 'jdk',
     package               => 'java-1.8.0-openjdk-devel',
     java_alternative      => 'jre-1.8.0-openjdk.x86_64',
     java_alternative_path => '/usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java',
   }
-}
