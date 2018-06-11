0ba0723e0af30e042e653b6835edc2967ea85bc8
Srikar Ananthula <ananthulasrikar@gmail.com>
2018-06-11 13:35:23 +0530

    Test oracle java

--- modules/profile/manifests/java.pp
+++ modules/profile/manifests/java.pp
@@ -1,8 +1,7 @@
 class profile::java {
-  class { '::java':
-    distribution          => 'jdk',
-    package               => 'java-1.8.0-openjdk-devel',
-    java_alternative      => 'jre-1.8.0-openjdk.x86_64',
-    java_alternative_path => '/usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java',
+  java::oracle { 'jdk6' :
+    ensure  => 'present',
+    version => '6',
+    java_se => 'jdk',
   }
 }
