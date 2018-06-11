647ad6aa909020bec6003b76d73f5cea006231c0
Srikar Ananthula <ananthulasrikar@gmail.com>
2018-06-11 13:03:23 +0530

    Test

--- modules/cron-puppet/manifests/init.pp
+++ modules/cron-puppet/manifests/init.pp
@@ -1,14 +1,7 @@
 class cron-puppet {
-    cron { 'puppet-apply':
-        ensure  => present,
-        command => "cd /etc/puppet ; /usr/bin/git pull",
-        user    => root,
-        minute  => '*/30',
-        require => File['post-hook'],
-    }
     user { 'srikar':
         ensure     => present,
-        uid        => $num,
+        uid        => 12345,
         shell      => '/bin/bash',
         home       => '/home/srikar'
     }
