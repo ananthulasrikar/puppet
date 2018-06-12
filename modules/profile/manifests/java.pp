a07822cbf086e81178127f714bbd7e5c786ef918
Srikar Ananthula <ananthulasrikar@gmail.com>
2018-06-12 11:33:16 +0530

    Fixed

--- modules/profile/manifests/java.pp
+++ modules/profile/manifests/java.pp
@@ -1,8 +1,8 @@
 class profile::java {
-  class { '::java':
-    distribution          => 'jdk',
-    package               => 'java-1.8.0-openjdk-devel',
-    java_alternative      => 'jre-1.8.0-openjdk.x86_64',
-    java_alternative_path => '/usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java',
+    class { '::java':
+      distribution          => 'jdk',
+      package               => 'java-1.8.0-openjdk-devel',
+      java_alternative      => 'jre-1.8.0-openjdk.x86_64',
+      java_alternative_path => '/usr/lib/jvm/jre-1.8.0-openjdk.x86_64/bin/java',
+    }
   }
-}
